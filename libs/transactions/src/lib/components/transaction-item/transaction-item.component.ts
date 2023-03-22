74e5ae5312a60aac9734531a8bf06c55e18c3d23
manisha <manisha@backbase.com>
2023-03-22 08:03:50 +0100

    test

--- libs/transactions/src/lib/components/transaction-item/transaction-item.component.ts
+++ libs/transactions/src/lib/components/transaction-item/transaction-item.component.ts
@@ -40,7 +40,7 @@ export class TransactionItemComponent implements OnChanges {
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    console.log("changes123458");
+    console.log("changes1234589");
     if (changes['transaction']) {
       this.amount = Number(
         this.transaction.transactionAmountCurrency.amount ?? 0
