99edb25a5549b294fc533b2c7db4322895a83731
manisha <manisha@backbase.com>
2023-04-06 14:45:40 +0200

    testing123

--- libs/transfer/src/lib/components/make-transfer-form/make-transfer-form.component.ts
+++ libs/transfer/src/lib/components/make-transfer-form/make-transfer-form.component.ts
@@ -44,7 +44,7 @@ export class MakeTransferFormComponent implements OnInit {
   }
 
   transfer(): void {
-    console.log("12345");
+    console.log("123455");
     if (this.makeTransferForm.valid) {
       this.submitTransfer.emit({
         fromAccount: this.makeTransferForm.value.fromAccount,
